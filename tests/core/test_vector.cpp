b4fc3f905a52b4b549aa5253ba6e05d8ff5bd9bc
Тимур Минажетдинов <timmin2303@mail.ru>
2025-06-12 22:08:47 +0300

    Added empty test.

---
+++ tests/core/test_vector.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Timmimin on 12.06.2025.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "vectrix/base_vector.h"
+
+TEST_CASE("Vector addition", "[vector]") {
+    vtx::vector<float, 3> a(1.0f, 2.0f, 3.0f);
+    vtx::vector<float, 3> b(1.0f);
+    auto c = a + b;
+    REQUIRE(c[0] == 2.0f);
+}
